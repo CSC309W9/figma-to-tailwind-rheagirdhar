c3e85bb0cc999b10e7265c2c6f3e5a82471df444
Rhea Girdhar <rheagirdhar@Rheas-MacBook-Pro.local>
2025-03-12 19:55:21 -0400

    DONE

--- app/page.js
+++ app/page.js
@@ -1,28 +1,45 @@
 // app/page.js
+// girdha16 - Rhea Girdhar
 export default function Home() {
   return (
-    <div className="w-[1769px] h-[1151px] relative bg-gradient-to-b from-[#db4439] to-[#75241e] overflow-hidden">
-      <div className="w-[150px] h-[52px] left-[1567px] top-[58px] absolute">
-        <div className="w-[150px] h-[52px] left-0 top-0 absolute bg-[#eaecd7] rounded-[49px]" />
-        <div className="left-[50px] top-0 absolute justify-start text-[#db4439] text-base font-black font-['Inknut_Antiqua']">Click</div>
+    <div className="w-full h-screen relative bg-gradient-to-b from-[#db4439] to-[#75241e] overflow-hidden">
+      {/* Button */}
+      <div className="absolute right-[5vw] top-[5vh]">
+        <button className="px-[2vw] py-[1vh] bg-[#eaecd7] rounded-full flex items-center justify-center">
+          <span className="text-[#db4439] text-[1.2vw] font-black font-['Inknut_Antiqua']">Login</span>
+        </button>
       </div>
-      <div className="origin-top-left rotate-[-8.64deg] w-[543px] h-[590.40px] left-[1068.69px] top-[226px] absolute">
-        <div className="w-[543px] h-[590.40px] left-[88.69px] top-0 absolute bg-[#d9d9d9]" />
-        <img className="w-[630.99px] h-[1054.71px] left-[81.51px] top-[-57.62px] absolute" src="https://placehold.co/631x1055" />
+      
+      {/* Coffee Image */}
+      <div className="origin-top-left rotate-[-8.64deg] w-[30vw] h-[50vh] absolute right-[15vw] top-[20vh]">
+        <div className="w-full h-full absolute" />
+        <img 
+          className="w-[28vw] h-auto absolute" 
+          src="https://marketplace.canva.com/EAE3ZQEPq2I/2/0/900w/canva-simple-aesthetic-coffee-time-instagram-instagram-BTsdS_U4hE4.jpg" 
+        />
       </div>
-      <div className="w-[680px] left-[143px] top-[313px] absolute justify-start text-[#eaecd7] text-[110px] font-black font-['Inknut_Antiqua'] leading-[134.20px]">This is the Topic This is the Topic</div>
-      <div className="w-[680px] left-[148px] top-[740px] absolute justify-start text-[#eaecd7] text-[40px] font-semibold font-['Inknut_Antiqua'] leading-[48.80px]">This is the Topic </div>
-      <div className="left-[270px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Me</div>
-      <div className="left-[64px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Front Page</div>
-      <div className="left-[891px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">ME</div>
-      <div className="left-[965px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">ME</div>
-      <div className="left-[716px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Front Page</div>
-      <div className="w-[1804px] h-[102px] left-[-35px] top-[1049px] absolute bg-white" />
-      <div className="left-[124px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
-      <div className="left-[459px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
-      <div className="left-[794px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
-      <div className="left-[1464px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
-      <div className="left-[1129px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
+      
+      {/* Main Heading */}
+      <div className="w-[38vw] absolute left-[8vw] top-[27vh] text-[#eaecd7] text-[6vw] font-black font-['Inknut_Antiqua'] leading-tight">
+        Yummy Coffee!
+      </div>
+      
+      {/* Subheading */}
+      <div className="w-[38vw] absolute left-[8vw] top-[64vh] text-[#eaecd7] text-[2.2vw] font-semibold font-['Inknut_Antiqua']">
+        Your #1 Choice 
+      </div>
+      
+      {/* Navigation */}
+      <div className="absolute left-[15vw] top-[4vh] text-[#eaecd7] text-[1.2vw] font-bold font-['Inknut_Antiqua']">Menu</div>
+      <div className="absolute left-[3.5vw] top-[4vh] text-[#eaecd7] text-[1.2vw] font-bold font-['Inknut_Antiqua']">Beans</div>
+      <div className="absolute left-[28vw] top-[4vh] text-[#eaecd7] text-[1.2vw] font-bold font-['Inknut_Antiqua']">Locations</div>
+      
+      {/* Footer */}
+      <div className="w-full h-[8vh] absolute bottom-0 left-0 bg-white">
+        <div className="absolute left-[3.5vw] top-[2vh] text-[#db4439] text-[1.2vw] font-black font-['Inknut_Antiqua']">Reviews</div>
+        <div className="absolute left-[17vw] top-[2vh] text-[#db4439] text-[1.2vw] font-black font-['Inknut_Antiqua']">Blogs</div>
+        <div className="absolute left-[31vw] top-[2vh] text-[#db4439] text-[1.2vw] font-black font-['Inknut_Antiqua']">Ratings</div>
+      </div>
     </div>
   );
 }
